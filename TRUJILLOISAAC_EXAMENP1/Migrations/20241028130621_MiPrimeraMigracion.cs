1ab57bca970d0bec6750e909a3d3f3cab37d46bc
Altermanns <isaac.277.trujillo@gmail.com>
2024-10-28 08:09:13 -0500

    Migration and Update database

---
+++ TRUJILLOISAAC_EXAMENP1/Migrations/20241028130621_MiPrimeraMigracion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TRUJILLOISAAC_EXAMENP1.Migrations
+{
+    /// <inheritdoc />
+    public partial class MiPrimeraMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Celular",
+                columns: table => new
+                {
+                    Idceluar = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    modelo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    año = table.Column<int>(type: "int", nullable: false),
+                    precio = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Celular", x => x.Idceluar);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ITrujillo",
+                columns: table => new
+                {
+                    IdUsuario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    numfav = table.Column<double>(type: "float", nullable: false),
+                    quierecambiar = table.Column<bool>(type: "bit", nullable: false),
+                    Idceluar = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ITrujillo", x => x.IdUsuario);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Celular");
+
+            migrationBuilder.DropTable(
+                name: "ITrujillo");
+        }
+    }
+}
