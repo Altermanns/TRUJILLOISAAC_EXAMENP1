dc6fa7e7819900e8eca196c27be3a627e1ef6b84
Altermanns <isaac.277.trujillo@gmail.com>
2024-10-28 07:35:38 -0500

    Primer commit

---
+++ TRUJILLOISAAC_EXAMENP1/Models/Celular.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TRUJILLOISAAC_EXAMENP1.Models
+{
+    public class Celular
+    {
+        [Required]
+        public int Idceluar { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string modelo { get; set; }
+        [Required]
+        public DateOnly año { get; set; }
+        public double precio { get; set; }
+
+    }
+}
---
+++ TRUJILLOISAAC_EXAMENP1/Models/ITrujillo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TRUJILLOISAAC_EXAMENP1.Models
+{
+    public class ITrujillo
+    {
+        [Required(ErrorMessage = "Complete este Campo")]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+        [Required]
+        public double numfav { get; set; }
+        public bool quierecambiar { get; set; }
+
+    }
+}
