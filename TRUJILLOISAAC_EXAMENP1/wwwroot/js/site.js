29112a19928489962e31d55b251d6259d4c1a20c
Altermanns <isaac.277.trujillo@gmail.com>
2024-10-28 08:54:34 -0500

    arreglos

--- TRUJILLOISAAC_EXAMENP1/wwwroot/js/site.js
+++ TRUJILLOISAAC_EXAMENP1/wwwroot/js/site.js
@@ -1,6 +1,6 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 document.getElementById('boton1').onclick = function () {
-    alert('Mi nombre es Isaac Altermann Trujillo  y mi hobbie es tocar la guitarra');
+    alert('Mi nombre es Isaac"Altermann"Trujillo  y mi hobbie es tocar la guitarra');
 }
 // Write your JavaScript code.
